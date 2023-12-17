bef98620bc49d455530954d259c550b99d8b96ab
devopsengineerme <sirishapasupulety@gmail.com>
2023-12-17 15:36:06 +0530

    third

---
+++ file3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+R=\e[31m]
+G=\e[32m]
+Y=\e[33m]
+n=\e[0m]
+person1=$1
+person2=$2
+echo " $R hi my name is $person1"
+echo " $G hey common what a surprise , I am $person2"
+Timestamp=$date +%F-%H-%M-%surprise
+if($? -ne 0)
+{$R check the script once}
+else
+{$Y success}
+
+
