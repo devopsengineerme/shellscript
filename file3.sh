1c8e2dfb4d1d1835d46718149c55255ab28efe24
devopsengineerme <sirishapasupulety@gmail.com>
2023-12-19 00:24:26 +0530

    update

--- file3.sh
+++ file3.sh
@@ -5,10 +5,11 @@ Y="\e[33m"
 N="\e[0m"
 person1=$1
 person2=$2
-echo " $R hi my name is $person1"
-echo " $G hey common what a surprise , I am $person2"
-Timestamp=$ date +%F-%H-%M-%s
+echo " ${R} hi my name is $person1"
+echo " ${G} hey common what a surprise , I am $person2"
+Timestamp=$ (date +%F-%H-%M-%s)
 if($? -ne 0)
-{$R check the script once}
+echo "${R} check the script once"
 else
-{$Y success}
+echo "${Y} success"
+fi
