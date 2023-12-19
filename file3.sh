99f1bafae1b0f27498652da45d4c93bb06bb05e3
devopsengineerme <sirishapasupulety@gmail.com>
2023-12-19 12:49:16 +0530

    third

--- file3.sh
+++ file3.sh
@@ -5,7 +5,7 @@ echo " $person2:hey common what a surprise , I am $person2"
 Timestamp=$(date) 
 echo " script started at : $Timestamp "
 test=$(echo $?)
-if[$test -ne 0]
+if[ $test -ne 0 ]
 then
 echo "check the script once"
 exit 1
