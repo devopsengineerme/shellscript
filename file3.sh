b76f9354ed47c71f9ece0486a6ae8404c978eeb0
devopsengineerme <sirishapasupulety@gmail.com>
2023-12-19 12:00:43 +0530

    third

--- file3.sh
+++ file3.sh
@@ -1,15 +1,11 @@
-#!/bin/bash
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
 person1=$1
 person2=$2
-echo " $\R hi my name is $person1"
-echo " $\G hey common what a surprise , I am $person2"
-Timestamp=$(date +%F-%H-%M-%s)
+echo " hi my name is $person1"
+echo "  hey common what a surprise , I am $person2"
+Timestamp=$(date) 
+echo " script started at : $(Timestamp) "
 if($? -ne 0)
-echo "$\R check the script once"
+echo "check the script once"
 else
-echo "${Y} success"
+echo "success"
 fi
